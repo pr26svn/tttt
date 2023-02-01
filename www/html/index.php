074475d48993c6d00e8035fd8d1498a4c1d28a02
pr26svn <pr26sautin@mail.ru>
2023-02-01 11:36:56 +0600

    main files

--- www/html/index.php
+++ www/html/index.php
@@ -4,4 +4,5 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Интернет-магазин \"Одежда\"");
 
 $APPLICATION->IncludeComponent("svn:user.adress","",[]);
+
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
